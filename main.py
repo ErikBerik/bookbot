5729379bb151a7e12c8b657043ef2fa7907bdf2f
ErikBerik <erik.j.berglund@hotmail.com>
2025-10-02 16:54:11 +0200

    refactoring so stats functions lie in a separate file

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import count_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
-
-def count_words(input_string):
-    words = input_string.split()
-    return len(words)
 
 def main():
     bookstring = get_book_text('books/frankenstein.txt')

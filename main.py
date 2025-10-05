fef8d56d7f57a164bca33ffb67e2c9cc7d4fcf05
ErikBerik <erik.j.berglund@hotmail.com>
2025-10-05 12:13:54 +0200

    made a pretty report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words,count_characters
+from stats import count_words,count_characters,sorted_list_of_dicts
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,10 +6,19 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    bookstring = get_book_text('books/frankenstein.txt')
+    bookpath = 'books/frankenstein.txt'
+    bookstring = get_book_text(bookpath)
     wordnumber = count_words(bookstring)
     character_counts = count_characters(bookstring)
+    character_dicts = sorted_list_of_dicts(character_counts) 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {bookpath}...")
+    print("----------- Word Count ----------")
     print(f'Found {wordnumber} total words')
-    print(character_counts)
+    print("--------- Character Count -------")
+    for item in character_dicts:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,13 @@ def count_characters(input_string):
         counts.setdefault(char,0)
         counts[char] += 1
     return counts
+
+def sorted_list_of_dicts(char_dict):
+    def sort_on(items):
+        return items["num"]
+    dictlist = []
+    for char,num in char_dict.items():
+        dictlist.append({"char":char,"num":num})
+    dictlist.sort(reverse=True, key=sort_on)
+    return dictlist
+    
